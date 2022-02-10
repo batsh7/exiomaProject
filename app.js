4b4c140553b87f889c05758f3e10ffbc26ad906b
sheinin <batsheva8180@gmail.com>
2022-02-10 22:31:55 +0200

    save

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const createError = require('http-errors');
-const express = require('express');
+const express = require('express'); 
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const {Log} = require('./collections/log')
+const requestIp = require('request-ip')
 
 /**
- * TODO:
+ * TODO: 
  * Change this to the real connection string!
  */
-db = mongoose.createConnection("mongodb://")
+// db = mongoose.createConnection("mongodb://mongo-t:fasdewr@157.245.75.184/persons?authSource=admin")
+  // db = mongoose.createConnection("mongodb://mongo-s:mongo1234@157.245.75.184/persons?authSource=admin")
+db = mongoose.createConnection("mongodb://localhost/persons");
 
+ 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users'); 
 
 const app = express();
 
@@ -43,5 +48,34 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+app.use('/', function (req, res, next) {
+  try {
+    let ip = requestIp.getClientIp(req)
+    let typeRequest = req.method
+    let date = new Date()
+    let newLog = await new Log({ ip: ip, requestType : typeRequest, date:date })
+    newLog = await newLog.save()
+    let now = new Date();
+    let sumMinuteAgo = new Date(now.getTime() - (1 * 60000));
+    let logs = await Log.find({ ip: ip, date: { $gte: sumMinuteAgo } })
+    let numLogs = logs.length;
+    if (numLogs == 10) {
+      let maxDate = await Log.findOne({}).sort({ date: -1 })
+      let difference = now.getTime() - maxDate.date.getTime();
+      let seconds = difference / 1000;
+      let seconds_Between_Dates = Math.abs(seconds);
+      let sec2 = 60 - seconds_Between_Dates;
+      return res.json({ code: 200, data: "You have exceeded the allowed number of requests, you will have to wait " + sec2 + " seconds" })
+    }
+    else {
+      next()
+    }
+  } catch (error) {
+    res.send(error)
+  }
+})
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -45,11 +45,11 @@ router.get('/populate-mongo', async (req, res, next) => {
 
 });
 router.get('/', async (req, res) => {
-    let p = await Person.findOne();
-    return res.json({code: 200, data: p})
+    let p = await Person.findOne(); 
+    return res.json({code: 200, data: p}) 
 })
 
-function getRandomInt(min, max) {
+function getRandomInt(min, max) {  
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
@@ -59,6 +59,104 @@ router.get('/', async (req, res) => {
     let person = await Person.findOne();
     let profession = await Profession.findOne();
     return res.json({code: 200, data: {person, profession}})
+})
+
+//===================//
+router.get('/a', async(req, res) =>{
+    let person = await Person.find( { profession : null } );
+    let avgPersons = await person.aggregate( [ { $group : { '_id': null}, 'avg': { $avg: '$age'} }])
+    return res.json({code: 200, data: {person,avgPersons}})
+})
+
+router.get('/b', async(req, res) => {
+    let profession = await Profession.findOne({}).sort({ salary : -1 }).limit(1)
+    let persons = await Person.find({ profession  : profession._id })
+    let numPersons = persons.length
+    return res.json({code: 200, data: profession, numPersons})
+})
+
+router.get('/c', async(req, res) => {
+    let salary = await Profession.find({}).sort( { salary : -1 }).limit(5)
+    return res.json({code: 200, data: {salary}})
+})
+
+router.get('/d', async(req, res) => {
+    let ages = await Person.distinct("age")
+    return res.json({code: 200, data: {ages}})
+})
+
+router.patch('/e', async(req, res) => {
+    let person = await Person.findOne( { name: req.body.name})
+    let profession
+    if (person.profession)
+         profession = await Profession.findOneAndUpdate({_id : person.profession},  {salary:req.body.salary}, {new:true})
+    return res.json({code: 200, data: {person, profession }})
+})
+  
+///////////////////
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+const multer = require('multer')
+const requsetIp = require('request-ip')
+
+///////////appendToFile////////
+
+let date_ob = new Date();
+
+router.get('/ip', async(req, res) =>{
+    let ip = requsetIp.getClientIp(req)
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+    let time = hours + ":" + minutes + ":" + seconds
+
+    fs.appendFile('file.txt', ip + " " + time + '\r\n', (err, data) => {
+        if(err) {
+            throw err;
+        }
+      return res.json({code: 200, data: {ip }})
+    })
+})
+
+/////upload image//////
+const imageStorage = multer.diskStorage({
+    // Destination to store image     
+    destination: 'public/', 
+      filename: (req, file, cb) => {
+          cb(null, file.fieldname + '_' + Date.now() 
+             + path.extname(file.originalname))
+            // file.fieldname is name of the field (image)
+            // path.extname get the uploaded file extension
+    }
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+    limits: {
+      fileSize: 1000000 // 1000000 Bytes = 1 MB
+    },
+    fileFilter(req, file, cb) {
+      if (!file.originalname.match(/\.(png|jpg)$/)) { 
+         // upload only png and jpg format
+         let ip = requsetIp.getClientIp(req)
+         fs.appendFile('file.txt', ip + " " + file.originalname + '\r\n', (err, data) => {
+            if(err) {
+                throw err;
+            }
+        })
+         return cb(new Error('Please upload a Image'))
+       }
+     cb(undefined, true)
+  }
+}) 
+
+// For Single image upload
+router.post('/uploadImage', imageUpload.single('image'), (req, res) => {
+    res.send(req.file)
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router;
